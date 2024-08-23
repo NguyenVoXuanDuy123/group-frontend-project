9f621287225f2dcdf95acb5e78cea1936a069852
nhatdongdang <144138246+nhatdongdang@users.noreply.github.com>
2024-08-23 12:45:06 +1000

    test

--- src/components/SideBarLeft/index.tsx
+++ src/components/SideBarLeft/index.tsx
@@ -1,5 +1,5 @@
 const SideBarLeft = () => {
-  return <div className="w-80 h-full bg-grey">SideBarLeft</div>;
+  return <div className="w-80 h-full bg-grey">SsideBarLeft</div>;
 };
 
 export default SideBarLeft;
