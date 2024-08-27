98c7f5abe787ab75f888b6e1bfff91add0c5f23c
NguyenVoXuanDuy123 <147340503+NguyenVoXuanDuy@users.noreply.github.com>
2024-08-27 17:32:59 +0700

    Change folder name Post to PostCard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import CreatePostPrompt from "@/components/home/CreatePostPrompt";
-import PostList from "@/components/Post/PostList";
+import PostList from "@/components/PostCard/PostList";
 import { fetchApi } from "@/helpers/fetchApi";
 import { Post } from "@/types/post.types";
 import { useEffect, useState } from "react";
